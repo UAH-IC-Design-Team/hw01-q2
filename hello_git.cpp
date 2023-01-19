964d910164db84900a15464c72477c4fd27023ea
unknown <apd0012@uah.edu>
2023-01-19 15:35:29 -0600

    Merged branches and removed conflict marekers this time'

--- hello_git.cpp
+++ hello_git.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 int main ()
 {
-	cout << "\nhello git\n";
-<<<<<<< HEAD
-	cout << "Creating a merge conflict\n";
-=======
+	cout << "\nhello git\n"
+
 	cout << "there's a change\n";
->>>>>>> origin/new_feature
+
 	return 0;
 }
