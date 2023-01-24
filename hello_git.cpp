55fc364e06c9e219e5c0d1238130a18f8c1efc3b
Anupriya Krishnamoorthy <anupriyakrishnamoorthy@Anupriyas-MacBook-Pro.local>
2023-01-24 15:49:06 -0600

    Trying to figure out some change

--- hello_git.cpp
+++ hello_git.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main ()
 {
 	cout << "\nhello git\n";
-	cout << "there's a change\n";
+	cout << "creating merge conflicts\n";
 	cout <<"adding a line to check if I am doing this correct\n";
 	return 0;
 }
